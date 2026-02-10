902a56a4fbdc9e858a7f62b8aaba9f6fc7ac8c50
KarlNoot <Luiscrm16@gmail.com>
2026-02-10 11:16:26 -0700

    I

--- entidades/pinguino.py
+++ entidades/pinguino.py
@@ -1,10 +1,10 @@
-class Gato:
+class Pinguino:
     def __init__ (self, nombre, edad, color):
         self.nombre = nombre
         self.edad = edad
         self.color = color
 
     def saludar (self):
-        #Esta funcion  va a hacer que el gato salude
+        #Esta funcion  va a hacer que el pinguino salude
         return f"Hola, me llamo {self.nombre}"
     
